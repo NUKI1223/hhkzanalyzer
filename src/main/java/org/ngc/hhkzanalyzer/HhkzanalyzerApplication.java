499cb83004c71630c690b57e7b0aaae54c6e35cd
NUKI1223 <mrdelron966@gmail.com>
2024-07-14 17:29:14 +0600

    Start project

---
+++ src/main/java/org/ngc/hhkzanalyzer/HhkzanalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.ngc.hhkzanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhkzanalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhkzanalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ngc/hhkzanalyzer/HhkzanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ngc.hhkzanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhkzanalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
