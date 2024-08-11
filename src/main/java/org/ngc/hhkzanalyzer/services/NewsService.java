bf61659c18f371f57832a00d77abf5a92193182d
NUKI1223 <mrdelron966@gmail.com>
2024-08-11 13:22:57 +0600

    we removed the errors from the news and made them in Russian

--- src/main/java/org/ngc/hhkzanalyzer/services/NewsService.java
+++ src/main/java/org/ngc/hhkzanalyzer/services/NewsService.java
@@ -8,11 +8,12 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NewsService {
     private static final String API_KEY = "7c41726b2d864118996e67a3ad2f5174";
-    private static final String API_URL = "https://newsapi.org/v2/everything?q=IT&language=en&apiKey=" + API_KEY;
+    private static final String API_URL = "https://newsapi.org/v2/everything?q=IT&language=ru&apiKey=" + API_KEY;
 
     public final RestTemplate restTemplate;
 
@@ -21,6 +22,16 @@ public class NewsService {
     }
     public List<News> getNews() {
         NewsApiResponse response = restTemplate.getForObject(API_URL, NewsApiResponse.class);
-        return response != null ? Arrays.asList(response.getArticles()) : List.of();
+        List<News> articles = response != null ? Arrays.asList(response.getArticles()) : List.of();
+        List<News> news = articles.size() > 15 ? articles.subList(0, 15) : articles;
+
+        return news.stream()
+                .filter(article -> !isInvalidArticle(article))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isInvalidArticle(News article) {
+        return article.getTitle().contains("[Removed]")
+                || article.getDescription().contains("[Removed]");
     }
 }
